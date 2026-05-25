8a23132a03efdb27520d7a21472ca62adf1366e6
Ray <iceui2016@gmail.com>
2026-05-25 19:49:51 +0800

    chore: bump to v1.2 (versionCode 3)

--- android/app-native/build.gradle.kts
+++ android/app-native/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.raycc.vidly.native"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     compileOptions {
