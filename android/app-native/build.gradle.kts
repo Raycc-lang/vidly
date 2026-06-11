6bb1904b34d7fab43434dcba784ebaf9a1c91d6a
Ray <iceui2016@gmail.com>
2026-06-11 11:43:08 +0800

    bump native to v1.6 (versionCode 7)

--- android/app-native/build.gradle.kts
+++ android/app-native/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.raycc.vidly.native"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 7
+        versionName = "1.6"
     }
 
     compileOptions {
